6df052d54e0a90e6052868f9ff26c2e04bcb4df5
felixding1989 <34818178+felixding1989@users.noreply.github.com>
2018-07-01 22:55:42 -0700

    commit

--- src/particle_filter.h
+++ src/particle_filter.h
@@ -10,6 +10,7 @@
 #define PARTICLE_FILTER_H_
 
 #include "helper_functions.h"
+#include <random>
 
 struct Particle {
 
@@ -30,7 +31,9 @@ class ParticleFilter {
 	// Number of particles to draw
 	int num_particles; 
 	
-	
+	//random generator
+	std::default_random_engine gen;
+
 	
 	// Flag, if filter is initialized
 	bool is_initialized;
